7c644d853dc253a1d44eb4c3d78216773d2269c9
SirBossDavid <davidsanchez4435@gmail.com>
2024-12-06 23:25:56 -0800

    part2

--- classifier.cpp
+++ classifier.cpp
@@ -5,32 +5,30 @@
 
 Classifier::Classifier() {}
 
-
 void Classifier::Train(const vector<Instance>& trainingData) {
     trainingInstances = trainingData;  
 }
 
 int Classifier::Test(const Instance& testInstance) {
     double minDistance = numeric_limits<double>::max();  
-    int predictedLabel = -1;
+    int predection = -1;
 
     for (int i = 0; i < trainingInstances.size(); ++i) { 
 
         double distance = EuclideanDistance(testInstance.features, trainingInstances.at(i).features);
         if (distance < minDistance) {
             minDistance = distance;
-            predictedLabel = trainingInstances.at(i).classifier;
+            predection = trainingInstances.at(i).classifier;
         }
     }
-
-    return predictedLabel;
+    return predection;
 }
 
 double Classifier::EuclideanDistance(const vector<double>& features1, const vector<double>& features2) {
-    double sum = 0.0;
+    double distance = 0.0;
     
     for (int i = 0; i < features1.size(); ++i) {
-        sum += pow(features1.at(i) - features2.at(i), 2);
+        distance += pow(features1.at(i) - features2.at(i), 2);
     }
-    return sqrt(sum);
+    return sqrt(distance);
 }
